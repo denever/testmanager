d7fccfff98956c98efc16b02b75a533e0b55cda5
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-10-25 15:04:18 +0200

    Added question insertion routine

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from sqlalchemy.orm import sessionmaker
+from model import engine, Alumn, AlumnClass, Question
+
 if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -27,3 +30,24 @@ if __name__ == '__main__':
         session.add(alumn)
         session.commit()
         loop = False if raw_input("Continue? ") in ("N",'n','No','no') else True
+
+    data = dict()
+    loop = True
+    while loop:
+        data['qtype'] = raw_input("Type: ")
+        data['question'] = raw_input("Question: ")
+        if data['qtype'] in ('BC','bc','b'):
+            data['answers'] = '\\item Vero\n\\item Falso'
+        else:
+            subloop = True
+            answer_id = 1
+            data['answers'] = str()
+            while subloop:
+                answer = raw_input('Answer %s:' % answer_id)
+                if not answer: break
+                data['answers'] += '\\item %s\n' % answer
+                print data['answers']
+                answer_id += 1
+        qa = Question(**data)
+        session.add(qa)
+        session.commit()
