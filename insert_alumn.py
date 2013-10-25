edd76238b1be555e07a655354c23a288e2cfce21
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-10-25 15:16:52 +0200

    Added routine to assign an alumn to a class

--- insert_alumn.py
+++ insert_alumn.py
@@ -14,7 +14,12 @@ if __name__ == '__main__':
         alumn = Alumn(**data)
         print 'Alumn %(surname)s %(name)s %(dsa)s created.' % data
         print alumn
-        alumn.belongs = session.query(AlumnClass).first()
+        print 'Assign to a class'
+        for cls in session.query(AlumnClass).order_by(AlumnClass.id):
+            print '\t', cls.id, cls.name
+
+        cls_id = raw_input('Select a class id: ')
+        alumn.belongs = session.query(AlumnClass).filter(AlumnClass.id == cls_id).first()
         session.add(alumn)
         session.commit()
         loop = False if raw_input("Continue? ") in ("N",'n','No','no') else True
