7a1e018af6c150b94242808b5583ad02b0096813
Giuseppe Martino <martinogiuseppe@gmail.com>
2013-10-27 19:45:36 +0100

    Added topic selection before question insertion

--- insert_question.py
+++ insert_question.py
@@ -1,9 +1,20 @@
+import sys
 from sqlalchemy.orm import sessionmaker
-from model import engine, Alumn, AlumnClass, Question
+from model import engine, Topic, Question
 
 if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    for topic in session.query(Topic).order_by(Topic.id):
+        print '\t', topic.id, topic.title, topic.did_unit
+
+    topic_id = raw_input("Select topic to add questions: ")
+    if not topic_id:
+        sys.exit(1)
+    topic_id = int(topic_id)
+    current_topic = session.query(Topic).filter(Topic.id == topic_id).first()
+
     data = dict()
     loop = True
     while loop:
@@ -21,6 +32,7 @@ if __name__ == '__main__':
                 data['answers'] += '\\item %s\n' % answer
                 answer_id += 1
         qa = Question(**data)
+        qa.topic = current_topic
         session.add(qa)
         session.commit()
         loop = False if raw_input("Continue? ") in ("N",'n','No','no') else True
