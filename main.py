b582fbf85108da89f7014c1d8e9a3cfd2af5c0a7
Giuseppe Martino <martinogiuseppe@gmail.com>
2014-01-29 04:49:51 +0100

    Used utils in main.py

--- main.py
+++ main.py
@@ -2,16 +2,17 @@ import random
 from datetime import datetime
 from sqlalchemy.orm import sessionmaker
 from model import engine, Alumn, AlumnClass, Question, Topic, Test, TestQuestionAssoc
+from utils import safe_prompt
 
 def create_test():
     print 'Create test for a class...'
-    title = raw_input('Test title: ')
+    title = safe_prompt(session, 'Test title: ')
     if not title: return False
 
     for cls in session.query(AlumnClass).order_by(AlumnClass.id):
         print '\t', cls.id, cls.name
 
-    cls_id = raw_input('Select a class id: ')
+    cls_id = safe_prompt(session, 'Select a class id: ')
 
     if not cls_id: return False
 
@@ -26,7 +27,7 @@ def create_test():
         for topic in session.query(Topic).order_by(Topic.id):
             print '\t', topic.id, topic.title, topic.did_unit
 
-        topic_id = raw_input('Select topics sequence [%s]: ' % ','.join(topic_ids))
+        topic_id = safe_prompt(session, 'Select topics sequence [%s]: ' % ','.join(topic_ids))
         if not topic_id: break
 
         topic = session.query(Topic).filter(Topic.id == int(topic_id)).first()
