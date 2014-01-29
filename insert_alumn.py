965236c1d9e9b8dc1ea5a0e8642058de36498a7b
Giuseppe Martino <martinogiuseppe@gmail.com>
2014-01-29 04:58:37 +0100

    Changed flow, first select class where to add alumns

--- insert_alumn.py
+++ insert_alumn.py
@@ -6,21 +6,22 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    print 'Select class where to add alumn'
+    current_class = None
+    while True:
+        current_class = select_class(session)
+        if not current_class: continue
+        break
+
     data = dict()
     while True:
         data['surname'] = safe_prompt(session, "Surname: ")
         data['name'] = safe_prompt(session, "Name: ")
         data['dsa'] = True if safe_prompt(session, "dsa: ") in ("Y",'y','Yes','yes') else False
         alumn = Alumn(**data)
+        alumn.belongs = current_class
         print 'Alumn %(surname)s %(name)s %(dsa)s created.' % data
         print alumn
-
-        cls = None
-        while True:
-            cls = select_class(session)
-            if not cls: continue
-            alumn.belongs = cls
-            break
         session.add(alumn)
         session.commit()
-        print_alumns(session, cls)
+        print_alumns(session, current_class)
